ea5cc26edce71ff323569d27e000e4a88b48002f
Fabricio Herrera <jfherrer@espol.edu.ec>
2019-08-19 19:05:16 -0500

    Añadiendo repo

---
+++ src/selector_automatizado_materias/Selector_Automatizado_Materias.java
@@ -0,0 +1,16 @@
+package selector_automatizado_materias;
+
+/**
+ *
+ * @author jfherrer
+ */
+public class Selector_Automatizado_Materias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
